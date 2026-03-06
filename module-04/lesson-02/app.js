d41fd13bbca8be36632172f5b02a694b1688c31e
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2026-03-06 15:46:50 +0200

    fix glass gard animation

--- module-04/lesson-02/app.js
+++ module-04/lesson-02/app.js
@@ -1,18 +1,22 @@
-const cardEl = document.querySelector(".js-card-overlay");
+const cardEl = document.querySelector(".js-card");
 
-cardEl.addEventListener("mousemove", handleMouseenter, true);
-cardEl.removeEventListener("mouseout", handleMouseenter);
+const { width, height } = cardEl.getBoundingClientRect();
+
+cardEl.addEventListener("mouseenter", event => {
+  event.target.addEventListener("mousemove", handleMouseenter);
+});
+
+cardEl.addEventListener("mouseout", event => {
+  event.target.style.transform =
+    "perspective(1000px) scale(1) rotateX(0) rotateY(0)";
+  event.target.removeEventListener("mousemove", handleMouseenter);
+});
 
 function handleMouseenter(event) {
-  event.preventDefault();
-
-  const height = event.target.clientHeight;
-  const width = event.target.clientWidth;
-
   requestAnimationFrame(() => {
     const xRotation = -30 * ((event.layerY - height / 2) / height);
     const yRotation = 20 * ((event.layerX - width / 2) / width);
 
-    event.target.style.transform = `perspective(1000px) scale(1.05) rotateX(${xRotation}deg) rotateY(${yRotation}deg)`;
+    event.target.style.transform = `perspective(1000px) scale(1.06) rotateX(${xRotation}deg) rotateY(${yRotation}deg)`;
   });
 }
